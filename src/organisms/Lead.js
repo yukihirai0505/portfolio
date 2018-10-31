49f174e4794a0b46ad5d2be2753b5aefdd79f10f
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-31 12:14:43 +0900

    Add animation

--- src/organisms/Lead.js
+++ src/organisms/Lead.js
@@ -10,19 +10,25 @@ class Lead extends Component {
     return (
       <div id="lead">
         <div id="lead-content">
-          <h1>Yuki Hirai</h1>
-          <h2>Software Engineer</h2>
+          <h1 className="animate seven">
+            <span>Y</span><span>u</span><span>k</span><span>i</span> &nbsp;
+            <span>H</span><span>i</span><span>r</span><span>a</span><span>i</span>
+          </h1>
+          <h2 className="animate five">
+            <span>S</span><span>o</span><span>f</span><span>t</span><span>w</span><span>a</span><span>r</span><span>e</span> &nbsp;
+            <span>E</span><span>n</span><span>g</span><span>n</span><span>e</span><span>e</span><span>r</span>
+          </h2>
           <a href="#" className="btn-rounded-white">
             Download Resume
           </a>
         </div>
         {/* End #lead-content */}
 
-        <div id="lead-overlay" />
+        <div id="lead-overlay"/>
 
         <div id="lead-down">
           <span>
-            <FontAwesomeIcon icon="chevron-down" />
+            <FontAwesomeIcon icon="chevron-down"/>
           </span>
         </div>
         {/* End #lead-down */}
