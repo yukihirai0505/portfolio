ab4861c19050df7c921e2b49a849643fd8f23305
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-31 18:11:19 +0900

    Update skills

---
+++ src/consts/skills.js
@@ -0,0 +1,12 @@
+export const skills = {
+  programming: [
+    'Java', 'Scala', 'PHP', 'Python', 'Node.js', 'Play Framework', 'Akka HTTP', 'WordPress'
+  ],
+  database: ['MySQL'],
+  operating: ['Linux', 'macOS'],
+  web: [
+    'HTML', 'CSS', 'JavaScript', 'jQuery', 'React.js', 'Redux', ''
+  ],
+  editor: ['Atom', 'Visual Studio Code', 'PHPStorm', 'IntelliJ', 'vim'],
+  others: ['GitHub', 'Nginx', 'AWS', 'New Relic', 'Docker', 'Jenkins', 'fabric', 'ansible', 'firebase']
+}
--- src/organisms/Skills.js
+++ src/organisms/Skills.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { skills } from '../consts/skills'
 
 class Skills extends Component {
   constructor(props) {
@@ -8,34 +9,29 @@ class Skills extends Component {
   render() {
     return (
       <div id="skills">
-        <h2 className="heading">Skills</h2>
+        <h2 className="heading">Programming Language</h2>
         <ul>
-          <li>JavaScript</li>
-          <li>Python</li>
-          <li>Ruby</li>
-          <li>Go</li>
-          <li>Node.js</li>
-          <li>AngularJs</li>
-          <li>React</li>
-          <li>Elixir</li>
-          <li>Java</li>
-          <li>C</li>
-          <li>C#</li>
-          <li>C++</li>
-          <li>Ruby on Rails</li>
-          <li>JavaScript</li>
-          <li>Python</li>
-          <li>Ruby</li>
-          <li>Go</li>
-          <li>Node.js</li>
-          <li>AngularJs</li>
-          <li>React</li>
-          <li>Elixir</li>
-          <li>Java</li>
-          <li>C</li>
-          <li>C#</li>
-          <li>C++</li>
-          <li>Ruby on Rails</li>
+          {skills.programming.map(e => <li>{e}</li>)}
+        </ul>
+        <h2 className="heading">Database</h2>
+        <ul>
+          {skills.database.map(e => <li>{e}</li>)}
+        </ul>
+        <h2 className="heading">Operating System</h2>
+        <ul>
+          {skills.operating.map(e => <li>{e}</li>)}
+        </ul>
+        <h2 className="heading">Web Technologies</h2>
+        <ul>
+          {skills.web.map(e => <li>{e}</li>)}
+        </ul>
+        <h2 className="heading">Editor/IDE</h2>
+        <ul>
+          {skills.editor.map(e => <li>{e}</li>)}
+        </ul>
+        <h2 className="heading">Others</h2>
+        <ul>
+          {skills.others.map(e => <li>{e}</li>)}
         </ul>
       </div>
     )
