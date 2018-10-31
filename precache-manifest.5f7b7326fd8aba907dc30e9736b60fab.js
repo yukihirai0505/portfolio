e3c388402d01b37547fd175c6cbd973349f9df6f
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-31 04:29:43 +0000

    Updates

---
+++ precache-manifest.5f7b7326fd8aba907dc30e9736b60fab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "6ae0cc5c5015f99dac90",
+    "url": "/portfolio/static/js/main.6ae0cc5c.chunk.js"
+  },
+  {
+    "revision": "b2af1557193fba98a14a",
+    "url": "/portfolio/static/js/1.b2af1557.chunk.js"
+  },
+  {
+    "revision": "6ae0cc5c5015f99dac90",
+    "url": "/portfolio/static/css/main.da8d7d17.chunk.css"
+  },
+  {
+    "revision": "e227090ef0f5446848f147987e085a0e",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.de506b83741993b54490238bd8b95559.js"
+  "/portfolio/precache-manifest.5f7b7326fd8aba907dc30e9736b60fab.js"
 );
 
 workbox.clientsClaim();
