e130e125df9a17f9c3330d55bfb40659ed45e5c7
Yuki Hirai <yukihirai0505@gmail.com>
2018-11-02 04:58:35 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.37959474622a0f8c537363b9e3f9e7a4.js"
+  "/portfolio/precache-manifest.7c8a1c0d87313b472b4e581246b40cb7.js"
 );
 
 workbox.clientsClaim();
