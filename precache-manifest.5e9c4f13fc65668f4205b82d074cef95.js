29ce3238701e90de68fa82ecb0e99dcc3e00fb10
Yuki Hirai <yukihirai0505@gmail.com>
2018-11-02 09:08:08 +0000

    Updates

---
+++ precache-manifest.5e9c4f13fc65668f4205b82d074cef95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "fe28e90782c2a8b0284f",
+    "url": "/portfolio/static/js/main.fe28e907.chunk.js"
+  },
+  {
+    "revision": "2bed961cfbe34766e98b",
+    "url": "/portfolio/static/js/1.2bed961c.chunk.js"
+  },
+  {
+    "revision": "fe28e90782c2a8b0284f",
+    "url": "/portfolio/static/css/main.7949b66b.chunk.css"
+  },
+  {
+    "revision": "ae18244f74f39e08a8b862ee31254e30",
+    "url": "/portfolio/index.html"
+  }
+];
