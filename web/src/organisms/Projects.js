3c352757a7c97ba38dfff1c6feb9a6c22d55c3fc
Yuki Hirai <yukihirai0505@gmail.com>
2018-11-03 13:32:03 +0900

    Update project translation

--- web/src/organisms/Projects.js
+++ web/src/organisms/Projects.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-constructor */
 import React, { Component } from 'react'
+import { withNamespaces } from 'react-i18next'
 
 class Projects extends Component {
   constructor(props) {
@@ -7,24 +8,26 @@ class Projects extends Component {
   }
 
   render() {
+    const { t } = this.props
     return (
       <div id="projects" className="background-alt">
-        <h2 className="heading">Projects</h2>
+        <h2 className="heading">{t('project.title')}</h2>
         <div className="container">
           <div className="row">
-            <div className="project shadow-large">
-              <div className="project-image">
-                <img
-                  src="https://yabaiwebyasan.com/wp-content/uploads/2018/10/parawan.jpg"
-                  alt={'yabai'}
-                />
-              </div>
-              <div className="project-info">
-                <h3>ほにゃ</h3>
-                <p>ほにゃほにゃ</p>
-                <button>にゃん</button>
-              </div>
-            </div>
+            {t('project.projects', { returnObjects: true }).map(e => {
+              return (
+                <div className="project shadow-large">
+                  <div className="project-image">
+                    <img src={e.img} alt={'yabai'}/>
+                  </div>
+                  <div className="project-info">
+                    <h3>{e.title}</h3>
+                    <p>{e.description}</p>
+                    {/*<button>View More</button>*/}
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
@@ -32,4 +35,4 @@ class Projects extends Component {
   }
 }
 
-export default Projects
+export default withNamespaces('translation')(Projects)
