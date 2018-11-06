f1d5ef4b21df688bdce8b0facd92595f6ec0d913
Yuki Hirai <yukihirai0505@gmail.com>
2018-11-06 10:12:46 +0000

    Updates

--- precache-manifest.5c9d3184bc1a7ca686ae9dd566f85dd9.js
+++ precache-manifest.5c9d3184bc1a7ca686ae9dd566f85dd9.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "6fcde4c1e49cd1b9cfdd",
-    "url": "/portfolio/static/js/main.6fcde4c1.chunk.js"
+    "revision": "bf9fa084a2587642f5c3",
+    "url": "/portfolio/static/js/main.bf9fa084.chunk.js"
   },
   {
     "revision": "43a7c1a93fd20aa22c16",
     "url": "/portfolio/static/js/1.43a7c1a9.chunk.js"
   },
   {
-    "revision": "6fcde4c1e49cd1b9cfdd",
-    "url": "/portfolio/static/css/main.09c01061.chunk.css"
+    "revision": "bf9fa084a2587642f5c3",
+    "url": "/portfolio/static/css/main.1615bf67.chunk.css"
   },
   {
-    "revision": "f40866c96a29a460049733cd9e425e94",
+    "revision": "6a2a1eeee44a02a82d088d22d9cf36f4",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6fb555cc662d8c230f4d6f73f38fdaf6.js"
+  "/portfolio/precache-manifest.5c9d3184bc1a7ca686ae9dd566f85dd9.js"
 );
 
 workbox.clientsClaim();
