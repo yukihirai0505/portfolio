546b801a51cb8e8e6701133275b65de6b93a326e
Yuki Hirai <yukihirai0505@gmail.com>
2019-06-06 12:07:21 +0000

    Updates

---
+++ precache-manifest.6d8f3c7c86fee2790ee81271d2a808c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d24f56d6390d0814aab37bf5ea8988",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "480bb19f4f6bebd03edc",
+    "url": "/portfolio/static/css/main.192895d6.chunk.css"
+  },
+  {
+    "revision": "aa8334bffaf1e02961b6",
+    "url": "/portfolio/static/js/2.c71372d2.chunk.js"
+  },
+  {
+    "revision": "480bb19f4f6bebd03edc",
+    "url": "/portfolio/static/js/main.fe3bf50a.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
